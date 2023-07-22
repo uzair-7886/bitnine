f66d0e22ac90267683beb3ac8f9f73647b7f18f6
uzair-7886 <jerry.khan7886@gmail.com>
2023-07-22 12:23:18 +0500

    initial commit

---
+++ Q5/backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cors =require('cors')
+
+
+const app = express()
+const PORT = 3001
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
